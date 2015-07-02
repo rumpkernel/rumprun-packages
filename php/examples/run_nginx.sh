8a0f02940491d8fcb0c6f5c8579386c69270d889
Martin Lucina <martin@lucina.net>
2015-07-02 23:02:04 +0200

    Update example runner scripts

--- php/examples/run_nginx.sh
+++ php/examples/run_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 rumprun xen -di -M 128 \
-    -n inet,static,10.0.120.200/24 \
-    -b images/data.iso,/data \
-    -b images/stubetc.iso,/etc \
-    -- ../nginx/bin/nginx.bin -c /data/conf/nginx.conf
+    -I net1,xenif -W net1,inet,static,10.0.120.200/24 \
+    -b ../images/data.iso,/data \
+    -b ../images/stubetc.iso,/etc \
+    -- ../../nginx/bin/nginx.bin -c /data/conf/nginx.conf
--- php/examples/run_php.sh
+++ php/examples/run_php.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 rumprun xen -di -M 128 \
-    -n inet,static,10.0.120.201/24 \
-    -b images/data.iso,/data \
+    -I net1,xenif -W net1,inet,static,10.0.120.201/24 \
+    -b ../images/data.iso,/data \
     -e PHP_FCGI_MAX_REQUESTS=0 \
-    -- bin/php-cgi.bin -b 8000
+    -- ../bin/php-cgi.bin -b 8000
