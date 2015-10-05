6a8e65f971297cbd6ee960cf98cddcc32fb0ef61
David Halls <dahalls@gmail.com>
2015-10-05 23:19:48 +0100

    Add Node.js for Rumprun

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -11,12 +11,17 @@ die()
 if [ $# -lt 2 ]; then
     echo "usage: apply-patches.sh TARGET [PATCH ...]" 1>&2
     echo "Applies all PATCHes listed on command line to the source tree at TARGET." 1>&2
+    echo "If TARGET is '' then strips 0 prefixes, otherwise strips 1 prefix." 1>&2
     exit 1
 fi
 TARGET=$1
 shift
 
-if [ ! -d ${TARGET} ]; then
+PREFIX=1
+if [ -z "${TARGET}" ]; then
+    TARGET=./
+    PREFIX=0
+elif [ ! -d ${TARGET} ]; then
     echo "error: ${TARGET}: not a directory" 1>&2
     exit 1
 fi
@@ -27,5 +32,5 @@ for patch in "$@"; do
         exit 1
     fi
     echo Applying ${patch}...
-    patch -d ${TARGET} -p1 -s < ${patch} || die
+    patch -d ${TARGET} -p${PREFIX} -s < ${patch} || die
 done
