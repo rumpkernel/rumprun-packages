3320734bb395b056dc76416f1b541e1818cb585a
Ryan Day <ryan@ryanday.net>
2015-11-09 12:19:20 -0500

    Fixes #44 Explicitly enable IPv6 during Python configuration. Update the example to display whether the socket supports IPv6.

--- python/examples/hw.py
+++ python/examples/hw.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import socket
 
 # Importing these verifies that our static modules built correctly
 import math
@@ -12,6 +13,10 @@ def hello_world():
     print("Welcome to rumprun/python %d.%d!" % sys.version_info[:2])
     print("{} / {}".format(sys.platform, os.name))
 
+    if socket.has_ipv6:
+        print("IPv6 is enabled")
+
+    print("\n" * 2)
 
 if __name__ == '__main__':
     hello_world()
