a8a97a57b5f2a26e41a7d9d94ae654869e9d2bfc
Ryan Day <ryan@ryanday.net>
2015-11-11 21:40:43 -0500

    Add sqlite support to the Python package

--- python/examples/hw.py
+++ python/examples/hw.py
@@ -6,15 +6,23 @@ import socket
 import math
 import ssl
 import zlib
+import json
 
 
 def hello_world():
     print("\n" * 4)
     print("Welcome to rumprun/python %d.%d!" % sys.version_info[:2])
     print("{} / {}".format(sys.platform, os.name))
+    print(json.dumps({'key': 'value'}))
 
     if socket.has_ipv6:
         print("IPv6 is enabled")
+
+    try:
+        import sqlite3
+        print("sqlite {} built correctly".format(sqlite3.sqlite_version))
+    except ImportError as e:
+        print("sqlite3 not built")
 
     print("\n" * 2)
 
