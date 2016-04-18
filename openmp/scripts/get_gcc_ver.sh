7c3b7878a79ca3b5d927bc489d084bbe3f11f1f9
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2016-04-18 17:22:33 +0200

    Fixed hopefully Travis build error
    Signed-off-by: Vincent Schwarzer <vincent.schwarzer@yahoo.de>

--- openmp/scripts/get_gcc_ver.sh
+++ openmp/scripts/get_gcc_ver.sh
@@ -6,8 +6,8 @@ if [ -z $1 ] ; then
 	exit 1;
 fi 
 
-gcc_ver=$($1 -v 2>&1 >/dev/null|grep -Po  '(?<=gcc version\s)([0-9]\.[0-9])');
-gcc_ver_check=$(echo $gcc_ver | cut -d '.' -f 1)
+gcc_ver=$($1 -v 2>&1 >/dev/null|grep "^gcc version"|cut -d' ' -f3);
+gcc_ver_check=$(echo $gcc_ver | cut -d '.' -f 1);
 if [ $(( $gcc_ver_check )) -eq 5 ] ; then 
 	gcc_branch='gcc-5-branch';
 	echo $gcc_branch;
